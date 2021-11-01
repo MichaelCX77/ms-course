3017bc1f5748eef93cbd8ef0c99d1130aa729eab
Michael Xavier <maycow_77@hotmail.com>
2021-10-31 22:12:45 -0300

    Login, jwt token

--- hr-oauth/src/main/java/com/devsuperior/hroauth/resources/UserResource.java
+++ hr-oauth/src/main/java/com/devsuperior/hroauth/resources/UserResource.java
@@ -1,13 +1,9 @@
 package com.devsuperior.hroauth.resources;
 
-import org.apache.http.protocol.HTTP;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.devsuperior.hroauth.entities.User;
 import com.devsuperior.hroauth.services.UserService;
--- hr-oauth/src/main/java/com/devsuperior/hroauth/services/UserService.java
+++ hr-oauth/src/main/java/com/devsuperior/hroauth/services/UserService.java
@@ -3,6 +3,9 @@ package com.devsuperior.hroauth.services;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.devsuperior.hroauth.entities.User;
@@ -10,7 +13,7 @@ import com.devsuperior.hroauth.feignclients.UserFeignClient;
 
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 	
 	@Autowired
 	private static Logger logger = LoggerFactory.getLogger(UserService.class);
@@ -28,6 +31,19 @@ public class UserService {
 		}
 		
 		logger.info("Email found: " + email);
+		return user;
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		
+		User user = userFeignClient.findByEmail(username).getBody();
+		if(user == null) {
+			logger.error("Email not found: " + username);
+			throw new UsernameNotFoundException("Email not found");
+		}
+		
+		logger.info("Email found: " + username);
 		return user;
 	}
 	
